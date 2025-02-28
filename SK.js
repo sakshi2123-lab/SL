b27f22afa185fb898a69b43a139faa688a5e4a92
sakshi2123-lab <sakshilavand33@gmail.com>
2025-02-28 13:29:45 +0530

    Create SK.js

---
+++ SK.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple List</title>
+</head>
+<body>
+    <h2>My Favorite Fruits</h2>
+    <ol>
+        <li>Apple</li>
+        <li>Banana</li>
+        <li>Orange</li>
+        <li>Grapes</li>
+    </ol>
+</body>
+</html>
